8a2d548a4969aa0928828f934affdf7b0f9f95e8
MousElPsyCongroo <mtapha.tall@gmail.com>
2024-12-04 00:41:19 +0100

    Add day02 part 2 missing edge cases

--- day02/solution.js
+++ day02/solution.js
@@ -98,6 +98,7 @@ function countSafeReportsWithOneTolerance(line) {
   let evolution = reports[0] - reports[1] > 0;
   let tolerance = 0;
   let skippedIndex = 0;
+  let next = 1;
 
   for (let i = 0; i < reports.length - 1; i++) {
     const currentDifference = reports[i] - reports[i + 1];
@@ -105,15 +106,23 @@ function countSafeReportsWithOneTolerance(line) {
     const currentIncrement = Math.abs(currentDifference);
 
     if (!isSafe(evolution, currentEvolution, currentIncrement)) {
-      if (tolerance > 2) {
+      if (!tolerance && i + 1 === reports.length - 1) {
+        safeReportsWithOneTolerance++;
         return;
       }
 
-      if (tolerance) {
-        i = skippedIndex + 1;
+      if (tolerance > 1) {
+        return;
       }
 
-      const differenceAfterSkip = reports[i - 1] - reports[i + 1];
+      //90 89 86 84 83 79
+
+      if (tolerance) {
+        i = skippedIndex + 1;
+        next = 0;
+      }
+
+      const differenceAfterSkip = reports[i - 1] - reports[i + next];
       const evolutionAfterSkip = differenceAfterSkip > 0;
       const incrementAfterSkip = Math.abs(differenceAfterSkip);
       evolution = evolutionAfterSkip;
